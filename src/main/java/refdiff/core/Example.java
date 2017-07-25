b77ae34c930629c280a9ce8266f4b9b858adf23a
andrehora <andrehoraa@gmail.com>
2017-07-25 14:29:04 -0400

    pass

--- src/main/java/refdiff/core/Example.java
+++ src/main/java/refdiff/core/Example.java
@@ -25,29 +25,46 @@ public class Example {
 		
 		String commit = line.split(",")[0];
 		String date = line.split(",")[1];
-		
 		RefDiff refDiff = new RefDiff();
 		GitService gitService = new GitServiceImpl();
 		try (Repository repository = gitService.cloneIfNotExists(folder, project)) {
 			List<SDRefactoring> refactorings = refDiff.detectAtCommit(repository, commit);
 		    for (SDRefactoring refactoring : refactorings) {
-		    	if ((refactoring.getName().equals("SameMethod") || refactoring.getName().equals("RenameMethod") || refactoring.getName().equals("MoveMethod")) && 
-		    			refactoring.getRemovedTypes().size() == 1 && refactoring.getAddedTypes().size() == 1) {
 			    	System.out.print(refactoring.getName());
-//			    	System.out.print(";");
-//			    	System.out.print(refactoring.getChangedCode());
-//			    	System.out.print(";");
-//			        System.out.print(refactoring.getEntityNameBefore());
-//			        System.out.print(";");
-//			        System.out.print(refactoring.getEntityNameAfter());
+			    	System.out.print(";");
+			    	System.out.print(refactoring.getChangedCode());
+			    	System.out.print(";");
+			        System.out.print(refactoring.getEntityNameBefore());
+			        System.out.print(";");
+			        System.out.print(refactoring.getEntityNameAfter());
 			        System.out.print(";");
 			        System.out.print(commit);
-//			        System.out.print(";");
-//			        System.out.println(date);
 			        System.out.print(";");
-			        System.out.print(refactoring.getRemovedTypes().get(0));
+			        System.out.println(date);
+		    	}
+		    }
+		catch (Exception ex) {
+			//System.out.println("Error:" + ex.getMessage());
+		}
+	}
+	
+	public static void apiMiningTransactionsAt(String project, String folder, String line) {
+		
+		String commit = line.split(",")[0];
+		RefDiff refDiff = new RefDiff();
+		GitService gitService = new GitServiceImpl();
+		try (Repository repository = gitService.cloneIfNotExists(folder, project)) {
+			List<SDRefactoring> refactorings = refDiff.detectAtCommit(repository, commit);
+		    for (SDRefactoring refactoring : refactorings) {
+		    	if (refactoring.getName().equals("SameMethod") || refactoring.getName().equals("RenameMethod") || refactoring.getName().equals("MoveMethod")) {  
+		    			//refactoring.getRemovedTypes().size() == 1 && refactoring.getAddedTypes().size() == 1) {
+			    	System.out.print(refactoring.getName());
 			        System.out.print(";");
-			        System.out.println(refactoring.getAddedTypes().get(0));
+			        System.out.print(commit);
+			        System.out.print(";");
+			        System.out.print(refactoring.getRemovedTypes());
+			        System.out.print(";");
+			        System.out.println(refactoring.getAddedTypes());
 		    	}
 		    }
 		}
@@ -58,18 +75,18 @@ public class Example {
 	
 	public static void main(String[] args) throws FileNotFoundException {
 		
-		String systemName = "clojure";
+		String systemName = "guava";
 		String project = "";
 		String systemFolder = "projects/"+systemName;
 		
-		PrintStream out = new PrintStream(new FileOutputStream("apimining_"+systemName));
+		PrintStream out = new PrintStream(new FileOutputStream("apimining2_"+systemName));
 		System.setOut(out);
 		
 		Path path = Paths.get("commits_"+systemName);
-		//Path path = Paths.get("commits");
 		
 		try (Stream<String> lines = Files.lines(path)) {
-            lines.forEach(line -> detectRefactoringAt(project, systemFolder, line));
+            //lines.forEach(line -> detectRefactoringAt(project, systemFolder, line));
+            lines.forEach(line -> apiMiningTransactionsAt(project, systemFolder, line));
         } catch (Exception ex) {
         	//System.out.println("Error:" + ex.getMessage());
         }
